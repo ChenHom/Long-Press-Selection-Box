36a26915dcee204c3dbe76d95b7206d0065e84ad
Chen Hom <orangecatchen@gmail.com>
2025-01-03 23:17:06 +0800

    修正註解，簡化清除選中盒子的說明

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener('mouseup', (e) => {
   if (isLongPress) {
     start = [];
   } else if (!e.target.classList.contains('box')) {
-    clearSelection(); // 點擊空白處清除所有選中的盒子
+    clearSelection(); // 點擊空白處清除所有選中
   }
   document.removeEventListener('mousemove', handleMove);
   isLongPress = false;
